135a7694a74e375d770dd49a0c5bf2b2fdc1f29e
Hiroshi Miura <miurahr@linux.com>
2026-05-29 23:58:42 +0900

    update: switch site configuration to production URL and root base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from 'astro/config';
 import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
-  // When going to production, please use the following URL
-  // site: 'https://www.osmf.jp',
-  // base: '/'
-  site: 'https://osmfj.github.io',
-  base: '/osmfj-new-website',
+  site: 'https://www.osmf.jp',
+  base: '/',
   markdown: {
     rehypePlugins: [
       [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
